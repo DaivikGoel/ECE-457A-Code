40242393814d25142da03bdf547ff5aca24d76b5
Bosco H <y95han@gmail.com>
2021-07-14 18:47:00 -0600

    Q1 complete

--- Assignment 3/Q1/config.py
+++ Assignment 3/Q1/config.py
@@ -1,14 +1,15 @@
 config = {
     'population_size' : 50,
     'mutation_probability' : .25,
-    'crossover_rate' : .6,
+    'crossover_rate' : .15,
 
-    'kp_low': 2,
+    # range of values for kp, td, and ti
+    'kp_low': 2, 
     'kp_high': 8,
     'td_low': 1.05,
     'td_high': 9.42,
     'ti_low': 0.26,
     'ti_high': 2.37,
 
-    'num_generations': 150, #TODO: change back to 150
+    'num_generations': 100, 
 }
---
+++ Assignment 3/Q1/readme.md
@@ -0,0 +1,8 @@
+To run:
+- install necesary libraries
+- directly run main.py
+
+
+the values to configure such as mutation probability, crossover rate, population size, and number of generations are in config.py   
+
+after all generations are complete, the plots will be made evaluating fitness scores vs generations
